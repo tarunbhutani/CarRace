85236f845baab241ce328ca15bb12061ddab24ce
tarun <tarunbhutani652@gmail.com>
2020-08-02 00:24:16 +0800

    Added trees on terrain

---
+++ Assets/Scripts/CameraStable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraStable : MonoBehaviour
+{
+    public GameObject carGameObject;
+    public float carX;
+    public float carY;
+    public float carZ;
+
+    // Update is called once per frame
+    void Update()
+    {
+        carX = carGameObject.transform.eulerAngles.x;
+        carY = carGameObject.transform.eulerAngles.y;
+        carZ = carGameObject.transform.eulerAngles.z;
+
+
+
+        transform.eulerAngles = new Vector3(carX - carX, carY, carZ - carZ);
+    }
+}
