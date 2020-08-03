5e71b4a20de8613c2cb058563fab5f1b1994c3f5
tarun <tarunbhutani652@gmail.com>
2020-08-04 00:58:25 +0800

    Create new button options game object in main menu scene

---
+++ Assets/Scripts/ButtonOptions.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonOptions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayGame() {
+        SceneManager.LoadScene(2);
+    }
+
+    public void TrackSelect() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void MainMenu() {
+        SceneManager.LoadScene(0);
+    }
+}
